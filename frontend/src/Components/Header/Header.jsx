5defa2ccff29e83fad5807b1db4c6f520f4accdd
FTMFR <fatemefarajzade018@gmail.com>
2024-04-26 19:53:57 +0330

    add (Landing): add Landing component and style

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TopBar from "./TopBar/TopBar";
 import NavBar from "./NavBar/NavBar";
+import Landing from "../Landing/Landing";
 
 const Header = () => {
   return (
     <div className="header">
       <TopBar />
       <NavBar />
+      <Landing />
     </div>
   );
 };
