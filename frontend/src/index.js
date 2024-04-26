d9e170912ad9bfb90edf281641ed3f498dc69de5
FTMFR <fatemefarajzade018@gmail.com>
2024-04-26 18:37:35 +0330

    add (index.js): import dependencies in index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import { BrowserRouter as Router } from "react-router-dom";
+import "./css/defaults.css";
+import "./css/fonts.css";
+import "./css/helpers.css";
+import "./css/reset.css";
+import "./css/variables.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,4 +15,3 @@ root.render(
     <App />
   </Router>
 );
-
