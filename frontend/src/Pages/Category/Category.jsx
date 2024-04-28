317993ec7841893dd86d7a7528a9647480e3d8b0
FTMFR <fatemefarajzade018@gmail.com>
2024-04-28 19:20:06 +0330

    add (Category): add Category pages for create  category page with react-router-dom

--- frontend/src/Pages/Category/Category.jsx
+++ frontend/src/Pages/Category/Category.jsx
@@ -1,10 +1,87 @@
-import React from 'react'
-import './category.css'
+import React from "react";
+import "./category.css";
+import TopBar from "../../Components/Header/TopBar/TopBar";
+import Footer from "../../Components/Footer/Footer";
+import NavBar from "../../Components/Header/NavBar/NavBar";
+import CourseBox from "../../Components/CourseBox/CourseBox";
+import Pagintaion from "../../Components/Pagination/Pagintaion";
 
 const Category = () => {
   return (
-    <div>Category</div>
-  )
-}
+    <>
+      <TopBar />
+      <NavBar />
 
-export default Category
+      <section class="courses">
+        <div class="container">
+          <div class="courses-top-bar">
+            <div class="courses-top-bar__right">
+              <div class="courses-top-bar__row-btn courses-top-bar__icon--active">
+                <i class="fas fa-border-all courses-top-bar__icon"></i>
+              </div>
+              <div class="courses-top-bar__column-btn">
+                <i class="fas fa-align-left courses-top-bar__icon"></i>
+              </div>
+
+              <div class="courses-top-bar__selection">
+                <span class="courses-top-bar__selection-title">
+                  مرتب سازی پیش فرض
+                  <i class="fas fa-angle-down courses-top-bar__selection-icon"></i>
+                </span>
+                <ul class="courses-top-bar__selection-list">
+                  <li class="courses-top-bar__selection-item courses-top-bar__selection-item--active">
+                    مرتب سازی پیش فرض
+                  </li>
+                  <li class="courses-top-bar__selection-item">
+                    مربت سازی بر اساس محبوبیت
+                  </li>
+                  <li class="courses-top-bar__selection-item">
+                    مربت سازی بر اساس امتیاز
+                  </li>
+                  <li class="courses-top-bar__selection-item">
+                    مربت سازی بر اساس آخرین
+                  </li>
+                  <li class="courses-top-bar__selection-item">
+                    مربت سازی بر اساس ارزان ترین
+                  </li>
+                  <li class="courses-top-bar__selection-item">
+                    مربت سازی بر اساس گران ترین
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="courses-top-bar__left">
+              <form action="#" class="courses-top-bar__form">
+                <input
+                  type="text"
+                  class="courses-top-bar__input"
+                  placeholder="جستجوی دوره ..."
+                />
+                <i class="fas fa-search courses-top-bar__search-icon"></i>
+              </form>
+            </div>
+          </div>
+
+          <div class="courses-content">
+            <div class="container">
+              <div class="row">
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+              </div>
+            </div>
+          </div>
+
+          <Pagintaion /> 
+
+        </div>
+      </section>
+
+
+      <Footer />
+    </>
+  );
+};
+
+export default Category;
