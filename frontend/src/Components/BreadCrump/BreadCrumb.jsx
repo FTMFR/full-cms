ba94977d8cf2df9d3cb69c7b0bcc45bf1636da17
FTMFR <fatemefarajzade018@gmail.com>
2024-05-02 14:45:39 +0330

    add (BreadCrumb): add BreadCrumb component for displat breadcrumb for courses and categories

---
+++ frontend/src/Components/BreadCrump/BreadCrumb.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./breadCrumb.css";
+import { Link } from "react-router-dom";
+
+const BreadCrumb = ({ links }) => {
+  return (
+    <section className="breadcrumb">
+      <div className="container">
+        <div className="breadcrumb__content">
+          <div className="breadcrumb__home-content-icon">
+            <i className="fas fa-home breadcrumb__home-icon"></i>
+          </div>
+          <ul className="breadcrumb__list">
+            {links.map((link) => (
+              <li className="breadcrumb__item" key={link.key}>
+                <Link to={`/${link.to}`} className="breadcrumb__link">
+                  {link.title}
+                  {link.id !== links.length ? (
+                    <i className="fas fa-angle-left breadcrumb__icon"></i>
+                  ) : null}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BreadCrumb;
--- frontend/src/Pages/CourseInfo/CourseInfo.jsx
+++ frontend/src/Pages/CourseInfo/CourseInfo.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './courseInfo.css'
+import React from "react";
+import "./courseInfo.css";
+import TopBar from "../../Components/Header/TopBar/TopBar";
+import NavBar from "../../Components/NavBar/NavBar";
+import Footer from "../../Components/Footer/Footer";
+import BreadCrumb from "../../Components/BreadCrump/BreadCrumb";
 
 const CourseInfo = () => {
   return (
-    <div>CourseInfo</div>
-  )
-}
+    <>
+      <TopBar />
+      <NavBar />
+      <BreadCrumb
+        links={[
+          { id: 1, title: "خانه", to: "" },
+          { id: 2, title: "آموزش برنامه نویسی فرانت اند", to: "category-info/frontend" },
+          { id: 3, title: "دوره متخصص جاوا اسکریپت", to: "course-info/js-expert" },
+        ]}
+      />
+      <Footer />
+    </>
+  );
+};
 
-export default CourseInfo
+export default CourseInfo;
