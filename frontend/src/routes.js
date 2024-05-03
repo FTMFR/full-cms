8ec11f0572430c61e872b292755b98f95c9d5a58
FTMFR <fatemefarajzade018@gmail.com>
2024-05-03 17:54:09 +0330

    add (Login/Register): add Login and Register page
    -Implement Login page for login users
    -Implement Register page for register users

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,15 +3,17 @@ import CourseInfo from "./Pages/CourseInfo/CourseInfo";
 import Category from "./Pages/Category/Category";
 import Article from "./Pages/Article/Article";
 import Courses from "./Pages/Courses/Courses";
-
-
+import Login from "./Pages/LoginPage/Login";
+import Register from "./Pages/Register/Register";
 
 const routes = [
-    {path: '/', element: <Index />},
-    {path: '/course-info/:courseName', element: <CourseInfo />},
-    {path: '/cateogory-info/:categoryName', element: <Category />},
-    {path: '/article-info/:articleName', element: <Article />},
-    {path: '/courses', element: <Courses />},
+  { path: "/", element: <Index /> },
+  { path: "/course-info/:courseName", element: <CourseInfo /> },
+  { path: "/cateogory-info/:categoryName", element: <Category /> },
+  { path: "/article-info/:articleName", element: <Article /> },
+  { path: "/courses", element: <Courses /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
 ];
 
 export default routes;
