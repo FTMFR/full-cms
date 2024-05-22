fc14951510c30a86bbc5d4f0dd1f8e25b4a8f22e
FTMFR <fatemefarajzade018@gmail.com>
2024-05-22 17:18:11 +0330

    add (LastCourses): add fetch api to LastCourse component for get data dynamically
    -Implement splice method to allCourse state

--- frontend/src/Components/CourseBox/CourseBox.jsx
+++ frontend/src/Components/CourseBox/CourseBox.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./courseBox.css";
 import CircleSpinner from "../CircleSpinner/CircleSpinner";
+import { Link } from "react-router-dom";
 
-const CourseBox = () => {
+const CourseBox = (props) => {
   const [isImgShow, setIsImgShow] = useState(false);
 
   const onImageLoader = () => {
@@ -13,29 +14,31 @@ const CourseBox = () => {
     console.log(error);
   };
 
+  console.log(props);
+
   return (
     <div className="col-4">
       <div className="course-box">
-        <a href="/">
+        <Link to={`/course-info/${props.shortName}`}>
           <img
-            src="/images/courses/fareelancer.png"
+            src={`./images/courses/${props.cover}`}
             alt="Course img"
             className="course-box__img"
             onLoad={onImageLoader}
             onError={(e) => onImageError(e)}
           />
           {!isImgShow && <CircleSpinner />}
-        </a>
+        </Link>
         <div className="course-box__main">
-          <a href="/" className="course-box__title">
-            دوره پروژه محور متخصص جنگو
-          </a>
+          <Link to={`/course-info/${props.shortName}`} className="course-box__title">
+            {props.name}
+          </Link>
 
           <div className="course-box__rating-teacher">
             <div className="course-box__teacher">
               <i className="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
               <a href="/" className="course-box__teacher-link">
-                رضا دولتی
+                {props.creator.name}
               </a>
             </div>
             <div className="course-box__rating">
@@ -70,17 +73,21 @@ const CourseBox = () => {
           <div className="course-box__status">
             <div className="course-box__users">
               <i className="fas fa-users course-box__users-icon"></i>
-              <span className="course-box__users-text">500</span>
+              <span className="course-box__users-text">
+                {props.discount}
+              </span>
             </div>
-            <span className="course-box__price">1,000,000</span>
+            <span className="course-box__price">
+              {props.price === 0 ? "رایگان" : props.price.toLocaleString()}
+            </span>
           </div>
         </div>
 
         <div className="course-box__footer">
-          <a href="/" className="course-box__footer-link">
+          <Link to={`/course-info/${props.shortName}`} className="course-box__footer-link">
             مشاهده اطلاعات
             <i className="fas fa-arrow-left course-box__footer-icon"></i>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
--- frontend/src/Components/LastCourse/LastCourse.jsx
+++ frontend/src/Components/LastCourse/LastCourse.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./lastCourse.css";
 import SectionHeader from "../SectionHeader/SectionHeader";
 import CourseBox from "../CourseBox/CourseBox";
 
 const LastCourse = () => {
+  const [allCourse, setAllCourse] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/v1/courses")
+      .then((res) => res.json())
+      .then((result) => {
+        setAllCourse(result);
+        console.log(allCourse);
+      });
+  }, []);
+
   return (
     <>
       <div className="course">
@@ -12,15 +23,15 @@ const LastCourse = () => {
             title="جدیدترین دوره ها"
             desc="سکوی پرتاب شما به سمت موفقیت"
             btnTitle="تمامی دوره ها"
-            href='courses'
+            href="courses"
           />
 
           <div className="course-content">
             <div className="container">
               <div className="row">
-                <CourseBox />
-                <CourseBox />
-                <CourseBox />
+                {allCourse.splice(0, 3).map((course) => (
+                  <CourseBox key={course._id} {...course} />
+                ))}
               </div>
             </div>
           </div>
