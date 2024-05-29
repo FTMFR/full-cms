964a5c879ec215069b362745dc696c093148ba74
FTMFR <fatemefarajzade018@gmail.com>
2024-05-29 16:08:46 +0330

    add (Courses): add Pagination section to Course page for paginate courses in courses page

--- frontend/src/Components/Pagination/Pagintaion.jsx
+++ frontend/src/Components/Pagination/Pagintaion.jsx
@@ -1,38 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./pagination.css";
+import { Link, useParams } from "react-router-dom";
 
-const Pagintaion = () => {
+const Pagintaion = ({ items, itemCount, pathname, setShowCourses }) => {
+  const [pageCount, setPageCount] = useState(0);
+  // const [click, setClick] = useState(false);
+  const [clickNum, setClickNum] = useState(0);
+  const { page } = useParams();
+
+  const clickHandler = (index) => {
+    // setClickNum(index);
+    console.log(clickNum);
+  };
+
+  useEffect(() => {
+    let endIndex = itemCount * page;
+    let startIndex = endIndex - itemCount;
+    let paginatedItems = items.slice(startIndex, endIndex);
+    setShowCourses(paginatedItems);
+    let pageNumbers = Math.ceil(items.length / itemCount);
+    setPageCount(pageNumbers);
+    // console.log(pageNumbers, pageCount);
+  }, [page]);
+
   return (
     <div className="courses-pagination">
       <ul className="courses__pagination-list">
-        <li className="courses__pagination-item">
-          <a href="/" className="courses__pagination-link">
-            <i className="fas fa-long-arrow-alt-right courses__pagination-icon"></i>
-          </a>
-        </li>
-        <li className="courses__pagination-item">
-          <a href="/" className="courses__pagination-link">
-            1
-          </a>
-        </li>
-        <li className="courses__pagination-item">
-          <a href="/" className="courses__pagination-link">
-            2
-          </a>
-        </li>
-        <li className="courses__pagination-item">
-          <a
-            href="/"
-            className="courses__pagination-link courses__pagination-link--active"
-          >
-            3
-          </a>
-        </li>
-        <li className="courses__pagination-item">
-          <a href="/" className="courses__pagination-link">
-            <i className="fas fa-long-arrow-alt-left courses__pagination-icon"></i>
-          </a>
-        </li>
+        {/* <li className="courses__pagination-item"> 
+         {clickNum !== 0 && (
+            <Link
+              to={`${pathname}/${clickNum + 1}`}
+              className="courses__pagination-link"
+              onClick={() => clickHandler(clickNum - 1)}
+            >
+              <i className="fas fa-long-arrow-alt-right courses__pagination-icon"></i>
+            </Link>
+          )}
+        </li> */}
+
+        {Array(pageCount)
+          .fill(0)
+          .map((item, index) => (
+            <li className="courses__pagination-item">
+              {index + 1 === Number(page) ? (
+                <Link
+                  to={`${pathname}/${index + 1}`}
+                  className="courses__pagination-link courses__pagination-link--active"
+                  // onClick={() => clickHandler(index)} 
+                >
+                  {index + 1}
+                </Link>
+              ) : (
+                <Link
+                  to={`${pathname}/${index + 1}`}
+                  className="courses__pagination-link"
+                  // onClick={() => clickHandler(index)}
+                >
+                  {index + 1}
+                </Link>
+              )}
+            </li>
+          ))}
+
+        {/* <li className="courses__pagination-item">
+          {clickNum + 1 !== pageCount && (
+            <Link
+              to={`${pathname}/${clickNum + 1}`}
+              className="courses__pagination-link"
+              onClick={() => clickHandler(clickNum + 1)}
+            >
+              <i className="fas fa-long-arrow-alt-left courses__pagination-icon"></i>
+            </Link>
+          )}
+        </li> */}
       </ul>
     </div>
   );
--- frontend/src/Pages/Courses/Courses.jsx
+++ frontend/src/Pages/Courses/Courses.jsx
@@ -5,9 +5,11 @@ import NavBar from "./../../Components/Header/NavBar/NavBar";
 import BreadCrumb from "./../../Components/BreadCrump/BreadCrumb";
 import Footer from "./../../Components/Footer/Footer";
 import CourseBox from "./../../Components/CourseBox/CourseBox";
+import Pagintaion from "../../Components/Pagination/Pagintaion";
 
 const Courses = () => {
   const [allCourse, setAllCourse] = useState([]);
+  const [showCourses, setShowCourses] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:4000/v1/courses")
@@ -34,45 +36,26 @@ const Courses = () => {
         ]}
       />
 
+      {/* <!--------------------------------  Courses-Section  --------------------------------> */}
+
       <section className="courses">
         <div className="container">
           <div className="courses-content">
             <div className="container">
               <div className="row">
-                {allCourse.map((course) => (
+                {showCourses.map((course) => (
                   <CourseBox {...course} key={course._id} />
                 ))}
               </div>
             </div>
           </div>
 
-          <div className="courses-pagination">
-            <ul className="courses__pagination-list">
-              <li className="courses__pagination-item">
-                <a href="/" className="courses__pagination-link">
-                  <i className="fas fa-long-arrow-alt-right courses__pagination-icon"></i>
-                </a>
-              </li>
-              <li className="courses__pagination-item">
-                <a
-                  href="/"
-                  className="courses__pagination-link courses__pagination-link--active"
-                >
-                  1
-                </a>
-              </li>
-              <li className="courses__pagination-item">
-                <a href="/" className="courses__pagination-link">
-                  2
-                </a>
-              </li>
-              <li className="courses__pagination-item">
-                <a href="/" className="courses__pagination-link">
-                  3
-                </a>
-              </li>
-            </ul>
-          </div>
+          <Pagintaion
+            items={allCourse}
+            itemCount={3}
+            pathname="/courses"
+            setShowCourses={setShowCourses}
+          />
         </div>
       </section>
 
