e87bb228249ff320d6ab4cfde9db88738b1fcb12
FTMFR <fatemefarajzade018@gmail.com>
2024-06-02 20:23:44 +0330

    add (PopularCourses): add PopularCourses components
    -Implement PopularCourses component for dispaly popular courses in a row with swiper slider package
    -Implement Popular page for display all popular courses in a page with pagination feature
    -Refactor presellCourse component name
    -Implement fetch API to PopularCourses and Popular page

--- frontend/src/Components/PopularCourses/PopularCourses.jsx
+++ frontend/src/Components/PopularCourses/PopularCourses.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./popularCourses.css";
 import SectionHeader from "../SectionHeader/SectionHeader";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import CourseBox from "../CourseBox/CourseBox";
 
 const PopularCourses = () => {
+  const [allPopular, setAllPopular] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/v1/courses/popular")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setAllPopular(result);
+      });
+  }, []);
+
+
   return (
     <>
       <div className="popular">
@@ -10,7 +26,22 @@ const PopularCourses = () => {
           <SectionHeader
             title="محبوب ترین دوره ها"
             desc="دوره های محبوب بر اساس امتیاز دانشجوها"
+            btnTitle="تمامی دوره ها"
+            href="/popularCourses/1"
           />
+        </div>
+        <div className="course-content">
+          <div className="container">
+            <div className="row">
+              <Swiper spaceBetween={50} slidesPerView={3}>
+                {allPopular.map((popular, index) => (
+                  <SwiperSlide key={index}>
+                    <CourseBox {...popular} isSlider={true} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- frontend/src/Pages/Index/Index.jsx
+++ frontend/src/Pages/Index/Index.jsx
@@ -4,7 +4,7 @@ import Header from "../../Components/Header/Header";
 import LastCourse from "../../Components/LastCourse/LastCourse";
 import AboutUs from "../../Components/AboutUs/AboutUs";
 import PopularCourses from "../../Components/PopularCourses/PopularCourses";
-import PreSellCourses from "../../Components/PreSell/PreSellCourses";
+import PreSellCourses from "../../Components/PreSellCourses/PreSellCourses";
 import LastArticles from "../../Components/LastArticles/LastArticles";
 import Footer from "../../Components/Footer/Footer";
 
