8a2337a9866f0c7cc0e2dca4659a155c0ca7d22c
FTMFR <fatemefarajzade018@gmail.com>
2024-07-02 15:51:25 +0330

    add (Header): add fetch api to get dynamic data from backend and pass them to Landing component

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopBar from "./TopBar/TopBar";
 import NavBar from "./NavBar/NavBar";
 import Landing from "../Landing/Landing";
 
 const Header = () => {
+  const [header, setHeader] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:4000/v1/infos/index")
+      .then((res) => res.json())
+      .then((result) => setHeader(result));
+  }, []);
   return (
     <div className="header">
-      <TopBar />
+      <TopBar infos={header}/>
       <NavBar />
-      <Landing />
+      <Landing infos={header} />
     </div>
   );
 };
