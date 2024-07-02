fdf648b579994991d1ba8b983545b72546a2099d
FTMFR <fatemefarajzade018@gmail.com>
2024-07-02 15:50:39 +0330

    refactor backend

--- backend/controllers/v1/infos.js
+++ backend/controllers/v1/infos.js
@@ -17,8 +17,8 @@ exports.getIndex = async (req, res) => {
   );
 
   res.json({
-    phone: allInfos[0].phone,
-    email: allInfos[0].email,
+    // phone: allInfos[0].phone,
+    // email: allInfos[0].email,
     coursesCount,
     usersCount,
     totalTime,
