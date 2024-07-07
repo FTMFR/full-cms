61801c51e734373e5d2d566236618054cfb569bd
FTMFR <fatemefarajzade018@gmail.com>
2024-07-07 11:42:40 +0330

    add (SideBar): add comments page to sidebar for shortcut

--- frontend/src/Components/AdminPanel/SideBar/SideBar.jsx
+++ frontend/src/Components/AdminPanel/SideBar/SideBar.jsx
@@ -67,7 +67,12 @@ const SideBar = () => {
             </Link>
           </li>
           <li>
-            <a href="/">
+            <Link to="comments">
+              <span>کامنت ها</span>
+            </Link>
+          </li>
+          <li>
+            <a href="offs">
               <span>کدهای تخفیف</span>
             </a>
           </li>
